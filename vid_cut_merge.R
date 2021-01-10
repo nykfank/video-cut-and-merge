65bd5f59399f8a92972c7fdce3b6e49f8c0a5d49
nykfank <nick@nyk.ch>
2021-01-11 00:17:07 +0100

    Update vid_cut_merge.R

--- vid_cut_merge.R
+++ vid_cut_merge.R
@@ -8,6 +8,7 @@ filme <- sort(list.files(".", pattern="mp4"))
 # Determine video resolutionution and FPS
 fps <- c()
 resolution <- c()
+sampling_rate <- c()
 for (f in filme) {
 	cmd <- sprintf('mediainfo --Inform="Video;%%FrameRate%%" %s 2>&1', f)
 	rf <- system(cmd, intern=TRUE)
@@ -17,17 +18,31 @@ for (f in filme) {
 	cmd <- sprintf('mediainfo --Inform="Video;%%Height%%" %s 2>&1', f)
 	rh <- system(cmd, intern=TRUE)
 	resolution <- c(resolution, sprintf("%s:%s", rw, rh))
+	cmd <- sprintf('mediainfo --Inform="Audio;%%SamplingRate%%" %s 2>&1', f)
+	rs <- system(cmd, intern=TRUE)
+	sampling_rate <- c(sampling_rate, as.numeric(rs))
 }
 
-# Determine most frequent resolutiontion and FPS
+# Determine most frequent video FPS
 fpst <- as.data.frame(table(fps), stringsAsFactors=FALSE)
 fpst <- fpst[order(fpst$Freq, decreasing=TRUE),]
 print(fpst, row.names=FALSE)
+writeLines("")
 top_fps <- round(as.numeric(fpst[1, "fps"]))
+
+# Determine most frequent video resolution
 resolutiont <- as.data.frame(table(resolution), stringsAsFactors=FALSE)
 resolutiont <- resolutiont[order(resolutiont$Freq, decreasing=TRUE),]
 print(resolutiont, row.names=FALSE)
+writeLines("")
 top_resolution <- resolutiont[1, "resolution"]
+
+# Determine most frequent audio sampling rate
+sampling_ratet <- as.data.frame(table(sampling_rate), stringsAsFactors=FALSE)
+sampling_ratet <- sampling_ratet[order(sampling_ratet$Freq, decreasing=TRUE),]
+print(sampling_ratet, row.names=FALSE)
+writeLines("")
+top_sampling_rate <- as.numeric(sampling_ratet[1, "sampling_rate"])
 
 # Read shift and long text files
 if (file.exists("shift.txt")) shift <- read.table("shift.txt", stringsAsFactors=FALSE)$V1 else shift <- NA
@@ -38,13 +53,14 @@ if (!dir.exists("cut")) dir.create("cut")
 for (f in filme) {
 	outfile <- sprintf("cut/%s", f)
 	if (file.exists(outfile)) next
-	start <- sum(f == shift) * nb_seconds
-	dauer <- nb_seconds + sum(f == lange) * nb_seconds
+	start <- sum(f == shift, na.rm=TRUE) * nb_seconds
+	dauer <- nb_seconds + sum(f == lange, na.rm=TRUE) * nb_seconds
 	writeLines(sprintf("%d/%d: %s %d to %d seconds", which(f == filme), length(filme), f, start, start+dauer))
 	if (re_encoding == TRUE) {
-		cmd <- sprintf("ffmpeg -hide_banner -loglevel panic -y -ss %d -i %s -t %d -vf scale=%s -codec:a aac -codec:v libx264 -crf 18 -profile:v main -r %d %s", start, f, dauer, top_resolution, top_fps, outfile)
+		cmd <- sprintf("ffmpeg -hide_banner -loglevel panic -y -ss %d -i %s -t %d -vf scale=%s -codec:a aac -b:a 128k -ar %d -af aresample=async=1000 -codec:v libx264 -crf 20 -profile:v main -r %d -avoid_negative_ts make_zero -fflags +genpts %s", 
+			start, f, dauer, top_resolution, top_sampling_rate, top_fps, outfile)
 	} else {
-		cmd <- sprintf("ffmpeg -hide_banner -y -ss %d -i %s -t %d -c copy -r 30 %s", start, f, dauer, outfile)	
+		cmd <- sprintf("ffmpeg -hide_banner -y -ss %d -i %s -t %d -c copy -r 30 -avoid_negative_ts make_zero -fflags +genpts %s", start, f, dauer, outfile)	
 	}
 	system(cmd)
 }
@@ -53,6 +69,6 @@ for (f in filme) {
 mergefiles <- sprintf("file '%s/cut/%s'", getwd(), filme)
 tempTextFile <- sprintf("/tmp/vidfiles%d.txt", as.integer(Sys.time()))
 write(mergefiles, file=tempTextFile)
-cmd <- sprintf('ffmpeg -hide_banner -loglevel panic -y -f concat -safe 0 -i %s -c copy %s', tempTextFile, merged_filename)
+cmd <- sprintf('ffmpeg -hide_banner -y -f concat -safe 0 -i %s -c copy %s', tempTextFile, merged_filename)
 writeLines(cmd)
 system(cmd)
